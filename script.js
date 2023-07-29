2d744ad58248254a96a4f32f8f1f9b938c67a4fe
Erik G Hilton <jujuweeks@ymail.com>
2023-07-29 12:31:03 -0400

    update

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ buttons.addEventListener("click", (e) => {
 
 function setColor() {
     if (count.innerHTML > 0) {
-      count.style.color = "yellow";
+    count.style.color = "yellow";
     } else if (count.innerHTML < 0) {
-      count.style.color = "orangered";
+    count.style.color = "orangered";
     } else {
         count.style.color = "#fff";
     }
