defe107eeff75fba5c73690171a1c32a7dbce192
Erik G Hilton <jujuweeks@ymail.com>
2023-07-29 13:14:38 -0400

    init commit

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-// const add = document.querySelector(".add");
-// const resetCount = document.querySelector(".reset");
-// const sub = document.querySelector(".subtract");
-const count = document.querySelector(".count");
-const buttons = document.querySelector(".buttons");
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
 
-buttons.addEventListener("click", (e) => {
-    if (e.target.classList.contains("add")) {
-        count.innerHTML++;
-        setColor();  
-    }
-    if (e.target.classList.contains("subtract")) {
-        count.innerHTML--;
-        setColor();  
-    }
-    if (e.target.classList.contains("reset")) {
-        count.innerHTML = 0;
-        setColor();  
-    }
-});
-
-function setColor() {
-    if (count.innerHTML > 0) {
-    count.style.color = "yellow";
-    } else if (count.innerHTML < 0) {
-    count.style.color = "orangered";
-    } else {
-        count.style.color = "#fff";
-    }
+const generateNumber = () => {
+    // Generate number btn 1 and 10
+   const randomNumber = Math.floor(Math.random() * 10 + 1);
 }
