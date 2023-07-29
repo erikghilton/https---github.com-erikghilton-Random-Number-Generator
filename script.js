537e69d12ff4ee1d0d4c4f99ab7c83f1a1333bc2
Erik G Hilton <jujuweeks@ymail.com>
2023-07-29 12:28:59 -0400

    update

--- script.js
+++ script.js
@@ -6,14 +6,25 @@ const buttons = document.querySelector(".buttons");
 
 buttons.addEventListener("click", (e) => {
     if (e.target.classList.contains("add")) {
-        count.innerHTML++;  
+        count.innerHTML++;
+        setColor();  
     }
     if (e.target.classList.contains("subtract")) {
-        count.innerHTML--;  
+        count.innerHTML--;
+        setColor();  
     }
     if (e.target.classList.contains("reset")) {
-        count.innerHTML = 0;  
+        count.innerHTML = 0;
+        setColor();  
     }
 });
 
-
+function setColor() {
+    if (count.innerHTML > 0) {
+      count.style.color = "yellow";
+    } else if (count.innerHTML < 0) {
+      count.style.color = "orangered";
+    } else {
+        count.style.color = "#fff";
+    }
+}
